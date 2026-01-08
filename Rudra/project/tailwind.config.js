1730d95e3a4f33eeeb063eb0447973d4aad1d7eb
RudraYBedekar <rudra.bedekar01@gmail.com>
2026-01-08 13:04:38 -0500

    update

--- Rudra/project/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'class', // REQUIRED for manual dark mode
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
